28eae7654bf5ac74a48d9b0a49b741d6a138e651
hagento <hagento@pik-potsdam.de>
2025-05-22 15:11:54 +0200

    adapt readHDDCDD

--- R/readHDDCDD.R
+++ R/readHDDCDD.R
@@ -27,10 +27,16 @@ readHDDCDD <- function(subtype = "ssp2") {
 
     # overwrite "historical" in ssp column with scenario
     df %>%
+      filter(.data$period >= 1990) %>%
       mutate(ssp = ifelse(.data$ssp == "historical", scen, .data$ssp),
              rcp = sub("\\.", "_", .data$rcp),
              value = replace_na(.data$value, 0))
   }))
+
+  # average historical data to avoid duplicates
+  data <- data %>%
+    group_by(across(-all_of("value"))) %>%
+    reframe(value = mean(.data$value))
 
   # prepare for output
   data <- data %>%
