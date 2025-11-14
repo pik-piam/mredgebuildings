3f83420d21eab43087853abc7fe22a73a4383981
robinhasse <robin.krekeler@pik-potsdam.de>
2025-11-14 11:53:48 +0100

    region-specific DH emission intensity

--- R/calcCarrierPrices.R
+++ R/calcCarrierPrices.R
@@ -17,8 +17,8 @@
 calcCarrierPrices <- function() {
 
   # temporal scope
-  periodsPast <- 2000:2022
-  periodsFuture <- seq(2025, 2100, 5)
+  periodsPast <- 2000:2025
+  periodsFuture <- seq(2030, 2100, 5)
   periods <- c(periodsPast, periodsFuture)
 
   # levels of future projections
@@ -165,7 +165,7 @@ calcCarrierPrices <- function() {
            value = case_when(!is.na(.data$value) ~ .data$value,
                              all(is.na(.data$value)) ~ .data$valueECEMF,
                              .default = .data$valueECEMF *
-                               (.data$value / .data$valueECEMF)[.data$period == .data$eod]),
+                                (.data$value / .data$valueECEMF)[.data$period == .data$eod]),
            variable = "price",
            unit = "USD/kWh",
            level = "central") %>%
@@ -180,13 +180,117 @@ calcCarrierPrices <- function() {
 
   ## past ====
 
-  # extremely rough assumption based on remind results
-  emiHeat <- data.frame(period  = c(2000, 2022),
-                        value   = c(4E-4, 3E-4),
-                        unit    = "t_CO2/kWh") %>%
+  # remind results
+  emiHeat <- inline.data.frame("region; period; value",
+                               "CAZ; 2005; 0.00038",
+                               "CAZ; 2010; 0.00046",
+                               "CAZ; 2015; 0.00051",
+                               "CAZ; 2020; 0.00036",
+                               "CAZ; 2025; 0.00022",
+                               "CHA; 2005; 0.00046",
+                               "CHA; 2010; 0.00042",
+                               "CHA; 2015; 0.00036",
+                               "CHA; 2020; 0.00033",
+                               "CHA; 2025; 0.00031",
+                               "DEU; 2005; 0.00031",
+                               "DEU; 2010; 0.00031",
+                               "DEU; 2015; 0.00032",
+                               "DEU; 2020; 0.00029",
+                               "DEU; 2025; 0.00027",
+                               "ECE; 2005; 0.00041",
+                               "ECE; 2010; 4e-04",
+                               "ECE; 2015; 0.00036",
+                               "ECE; 2020; 3e-04",
+                               "ECE; 2025; 2e-04",
+                               "ECS; 2005; 0.00034",
+                               "ECS; 2010; 0.00035",
+                               "ECS; 2015; 0.00034",
+                               "ECS; 2020; 0.00029",
+                               "ECS; 2025; 0.00023",
+                               "ENC; 2005; 0.00015",
+                               "ENC; 2010; 0.00013",
+                               "ENC; 2015; 7.3e-05",
+                               "ENC; 2020; 3.1e-05",
+                               "ENC; 2025; 6.7e-06",
+                               "ESC; 2005; 0.00024",
+                               "ESC; 2010; 0.00023",
+                               "ESC; 2015; 0.00022",
+                               "ESC; 2020; 0.00018",
+                               "ESC; 2025; 0.00013",
+                               # "ESW; 2005; 0.00091",
+                               "ESW; 2010; 0.00025",
+                               "ESW; 2015; 6.2e-05",
+                               "ESW; 2020; 8.9e-06",
+                               "ESW; 2025; 5.4e-07",
+                               "EWN; 2005; 0.00023",
+                               "EWN; 2010; 0.00025",
+                               "EWN; 2015; 0.00025",
+                               "EWN; 2020; 0.00024",
+                               "EWN; 2025; 0.00021",
+                               "FRA; 2005; 2e-04",
+                               "FRA; 2010; 2e-04",
+                               "FRA; 2015; 0.00019",
+                               "FRA; 2020; 0.00012",
+                               "FRA; 2025; 6.7e-05",
+                               "IND; 2010; 0",
+                               "IND; 2015; 0.00025",
+                               "IND; 2020; 0.00044",
+                               "IND; 2025; 0.00045",
+                               "JPN; 2005; 0.00017",
+                               "JPN; 2010; 2e-04",
+                               "JPN; 2015; 0.00022",
+                               "JPN; 2020; 0.00026",
+                               "JPN; 2025; 0.00029",
+                               "LAM; 2010; 0",
+                               "LAM; 2015; 1.4e-05",
+                               "LAM; 2020; 9.3e-05",
+                               "LAM; 2025; 0.00024",
+                               "MEA; 2010; 0",
+                               "MEA; 2015; 5.5e-06",
+                               "MEA; 2020; 0.00025",
+                               "MEA; 2025; 4e-04",
+                               "NEN; 2005; 6e-05",
+                               "NEN; 2010; 9.4e-05",
+                               "NEN; 2015; 0.00014",
+                               "NEN; 2020; 0.00015",
+                               "NEN; 2025; 0.00014",
+                               "NES; 2005; 9.7e-05",
+                               "NES; 2010; 0.00011",
+                               "NES; 2015; 0.00012",
+                               "NES; 2020; 0.00013",
+                               "NES; 2025; 0.00014",
+                               "OAS; 2005; 3e-04",
+                               "OAS; 2010; 3e-04",
+                               "OAS; 2015; 3e-04",
+                               "OAS; 2020; 0.00032",
+                               "OAS; 2025; 0.00028",
+                               "REF; 2005; 0.00033",
+                               "REF; 2010; 0.00033",
+                               "REF; 2015; 0.00032",
+                               "REF; 2020; 0.00031",
+                               "REF; 2025; 0.00029",
+                               "SSA; 2010; 0",
+                               "SSA; 2015; 0",
+                               "SSA; 2020; 5e-05",
+                               "SSA; 2025; 0.00015",
+                               "UKI; 2005; 4e-04",
+                               "UKI; 2010; 0.00034",
+                               "UKI; 2015; 0.00027",
+                               "UKI; 2020; 0.00021",
+                               "UKI; 2025; 0.00014",
+                               "USA; 2005; 0.00037",
+                               "USA; 2010; 0.00029",
+                               "USA; 2015; 0.00021",
+                               "USA; 2020; 0.00017",
+                               "USA; 2025; 0.00015") %>%
     interpolate_missing_periods(periodsPast, expand.values = TRUE) %>%
     filter(.data[["period"]] %in% periodsPast) %>%
-    mutate(carrier = "heat")
+    right_join(toolGetMapping("regionmapping_21_EU11.csv", "regional", "mappingfolder"),
+               by = c(region = "RegionCode"),
+               relationship = "many-to-many") %>%
+    select(region = "CountryCode", "period", "value") %>%
+    mutate(unit = "t_CO2/kWh",
+           carrier = "heat")
 
   # historic emission intensity from EEA
   emiElec <- readSource("EEA_EuropeanEnvironmentAgency",
@@ -226,18 +330,13 @@ calcCarrierPrices <- function() {
   # assume long-term residual emission intensity
   emi <- expand.grid(carrier = c("elec", "heat"),
                      level = lvs) %>%
-    mutate(period = ifelse(.data[["level"]] == "low",
-                           ifelse(.data[["carrier"]] == "elec",
-                                  2040,
-                                  2045),
-                           2050),
-           factor = ifelse(.data[["level"]] == "low",
-                           0,
-                           ifelse(.data[["level"]] == "central",
-                                  ifelse(.data[["carrier"]] == "elec",
-                                         0.1,
-                                         0.2),
-                                  1))) %>%
+    mutate(period = case_when(.data$level == "low" & .data$carrier == "elec" ~ 2040,
+                              .data$level == "low" & .data$carrier == "heat" ~ 2045,
+                              .default = 2050),
+           factor = case_when(.data$level == "low"                               ~ 0,
+                              .data$level == "central" & .data$carrier == "elec" ~ 0.1,
+                              .data$level == "central" & .data$carrier == "heat" ~ 0.2,
+                              .default = 1)) %>%
     complete(carrier = unique(emi$carrier),
              level = lvs,
              fill = list(factor = 1, period = max(periodsFuture))) %>%
@@ -257,7 +356,6 @@ calcCarrierPrices <- function() {
 
 
   # Combine data ---------------------------------------------------------------
-
   data <- rbind(prices, emi)
 
   # all carriers included?
